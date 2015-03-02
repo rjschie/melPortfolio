9731933370ab326a562fae6a71b60feec5d5669b
Ryan Schie <rjschie@gmail.com>
2015-03-01 20:44:08 -0800

    set up resources for 'slugs'

--- api/app/models/DesignGallery.php
+++ api/app/models/DesignGallery.php
@@ -2,7 +2,7 @@
 
 class DesignGallery extends Eloquent {
 
-	protected $fillable = [ 'title', 'link','image' ];
+	protected $fillable = [ 'title', 'short_title', 'slug','image' ];
 
 	public $table = 'design_galleries';
 	public $timestamps = false;
