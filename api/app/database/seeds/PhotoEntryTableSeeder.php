0e595181b30d5c6e061a1c423c2181e574fe32b5
Ryan Schie <rjschie@gmail.com>
2015-03-04 21:18:54 -0800

    seed `sort_pos` in photo table seeders

--- api/app/database/seeds/PhotoEntryTableSeeder.php
+++ api/app/database/seeds/PhotoEntryTableSeeder.php
@@ -7,26 +7,27 @@ class PhotoEntryTableSeeder extends Seeder {
 		DB::table('photo_entries')->delete();
 		DB::unprepared("ALTER TABLE `photo_entries` AUTO_INCREMENT = 1;");
 
-		$arr = [
-			'lifestyle',
-			'portraits',
-			'editorial',
-			'travel'
-		];
+		$arr = PhotoGallery::orderBy('sort_pos', 'ASC')->lists('slug');
 
 		foreach($arr as $key => $gall) {
 
+			$randoArr = [];
 			$dir = '/var/www/http/_projects/melPortfolio/dev/assets/imgs/'.$gall;
 
 			if (is_dir($dir)) {
 				$theFiles = scandir($dir);
 				$theFiles = array_diff($theFiles, array('..', '.', '.DS_Store'));
 
+				for($i=1; $i <= count($theFiles); $i++)
+					$randoArr[] = $i;
+				shuffle($randoArr);
+
 				foreach($theFiles as $file) {
 					PhotoEntry::create([
 						'photo_gallery_id' => $key+1,
 						'title' => ucwords(explode('.',$file)[0]),
-						'image' => $file
+						'image' => $file,
+						'sort_pos' => array_shift($randoArr)
 					]);
 				}
 			}
