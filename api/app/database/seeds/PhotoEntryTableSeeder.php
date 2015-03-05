5594f9706af47de02ed5fb60b42e5908cf1bdcd2
Ryan Schie <rjschie@gmail.com>
2015-03-04 22:34:54 -0800

    include folder in 'image' column's seed data

--- api/app/database/seeds/PhotoEntryTableSeeder.php
+++ api/app/database/seeds/PhotoEntryTableSeeder.php
@@ -26,7 +26,7 @@ class PhotoEntryTableSeeder extends Seeder {
 					PhotoEntry::create([
 						'photo_gallery_id' => $key+1,
 						'title' => ucwords(explode('.',$file)[0]),
-						'image' => $file,
+						'image' => $gall.'/'.$file,
 						'sort_pos' => array_shift($randoArr)
 					]);
 				}
