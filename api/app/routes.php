dee76713d45aa84c8dfbeb7160aa0b9ac12c45ba
Ryan Schie <rjschie@gmail.com>
2015-05-28 19:07:44 -0700

    API: restrict routes
    Filter Routes to return token exceptions from 'jwt-auth'

--- api/app/routes.php
+++ api/app/routes.php
@@ -11,26 +11,31 @@
 |
 */
 
-Route::get('/', function() {
-	return 'Functioning';
-});
-
+/**
+ * For Scratch information during testing
+ * TODO: remove after completion
+ */
 if(App::environment('local')) {
 	Route::get('/scratch', 'ScratchPadController@index');
 }
 
+
+/**
+ * Main Routes for GET requests
+ */
 Route::post('/login', 'AuthController@authenticate');
-
-Route::resource('design_galleries', 'DesignGalleryController',
-	['only' => ['index', 'show', 'store', 'update', 'destroy']]
-);
-
-Route::resource('design_entries', 'DesignEntryController',
-	['only' => ['store', 'update', 'destroy']]
-);
-
-Route::resource('photo_galleries', 'PhotoGalleryController',
-	['only' => ['index', 'show', 'store', 'update', 'destroy']]
-);
-
+Route::resource('design_galleries', 'DesignGalleryController', ['only' => ['index', 'show']]);
+Route::resource('photo_galleries', 'PhotoGalleryController', ['only' => ['index', 'show']]);
 Route::get('/photo_random', 'PhotoGalleryController@random');
+
+
+/**
+ * Route group for authorized token
+ */
+Route::group(['before' => 'jwt-auth'], function() {
+
+	Route::resource('design_galleries', 'DesignGalleryController', ['only' => ['store', 'update', 'destroy']]);
+	Route::resource('design_entries', 'DesignEntryController', ['only' => ['store', 'update', 'destroy']]);
+	Route::resource('photo_galleries', 'PhotoGalleryController', ['only' => ['store', 'update', 'destroy']]);
+
+});
