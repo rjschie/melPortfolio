669c7c119e2b1bb5d035a4bd2b7b48397a342d54
Ryan Schie <rjschie@gmail.com>
2015-05-31 16:38:42 -0700

    implement refreshToken
    refreshToken happens once everyday, and returns new token within the TTL (extended to 1 week)

--- dev/js/ng-services.js
+++ dev/js/ng-services.js
@@ -5,26 +5,18 @@ angular.module('app.services', [])
 .factory('AuthServ', ['$http', '$q', '$window',
 	function($http, $q, $window) {
 
-		function login(email, password) {
+		function parsePayload(jwt) {
+			var payload = jwt.split('.')[1];
+			return JSON.parse(window.atob(payload));
+		}
+
+		function refreshToken() {
 			var deferred = $q.defer();
 
-			$http.post("../api/login", {
-				email: email,
-				password: password
-			}).then(function(res) {
-				var sessionInfo = { token : res.data.token };
-				$window.localStorage['sessionInfo'] = JSON.stringify(sessionInfo);
-				deferred.resolve(sessionInfo);
+			$http.post("../api/refreshToken").then(function(res) {
+				$window.localStorage.setItem('sessionInfo', JSON.stringify({token:res.data.token}));
+				deferred.resolve();
 			}, function(res) {
-
-				switch(res.data.error) {
-					case 'invalid_credentials':
-						res.data.error = "Wrong username or password.";
-						break;
-					default:
-						res.data.error = "Couldn't Log In.";
-						break;
-				}
 				deferred.reject(res);
 			});
 
@@ -32,7 +24,31 @@ angular.module('app.services', [])
 		}
 
 		return {
-			login: login,
+			login: function(email, password) {
+				var deferred = $q.defer();
+
+				$http.post("../api/login", {
+					email: email,
+					password: password
+				}).then(function(res) {
+					var sessionInfo = { token : res.data.token };
+					$window.localStorage['sessionInfo'] = JSON.stringify(sessionInfo);
+					deferred.resolve(sessionInfo);
+				}, function(res) {
+
+					switch(res.data.error) {
+						case 'invalid_credentials':
+							res.data.error = "Wrong username or password.";
+							break;
+						default:
+							res.data.error = "Couldn't Log In.";
+							break;
+					}
+					deferred.reject(res);
+				});
+
+				return deferred.promise;
+			},
 			logout: function() {
 				delete $window.localStorage.sessionInfo;
 				return ($window.localStorage.sessionInfo) ? true : false;
@@ -40,6 +56,21 @@ angular.module('app.services', [])
 			authenticate: function() {
 
 				if($window.localStorage.sessionInfo && JSON.parse($window.localStorage.sessionInfo).hasOwnProperty('token')) {
+					var jwt = JSON.parse($window.localStorage.sessionInfo).token;
+					var payload = parsePayload(jwt);
+					var now = (Date.now() / 1000);
+
+					if( now >= parseInt(payload.exp) ) return false;
+
+					if( now > (parseInt(payload.iat) + 8600)) {
+						refreshToken().then(function() {
+							return true;
+						}, function(res) {
+							console.log("Refresh Failure: " + res.data.error);
+							return false;
+						});
+					}
+
 					return true;
 				} else {
 					return false;
