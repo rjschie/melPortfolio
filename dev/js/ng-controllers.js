e9169d26cf2d2aa6f62fb9af61391a5df1d64ec5
Ryan Schie <rjschie@gmail.com>
2015-07-13 16:44:45 -0700

    clear() fix: image uploader graceful cldear

--- dev/js/ng-controllers.js
+++ dev/js/ng-controllers.js
@@ -192,6 +192,9 @@ angular.module('app.controllers', [])
 				galleryList.forEach(function(gallery, key) {
 					if(gallery.slug == $scope.$stateParams.gallerySlug) {
 						$scope.formData = angular.copy(gallery);
+						if($scope.formData.image) {
+							$scope.formData.image = 'uploads/design-home/' + $scope.formData.image;
+						}
 						$scope.model = $scope.$parent.galleries.design;
 						$scope.index = key;
 					}
