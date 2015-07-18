187f50e22512a3d2d70ee29b18abe00d5afa6c1c
Ryan Schie <rjschie@gmail.com>
2015-07-17 17:51:07 -0700

    cleanup ngDirectives comments/code layout

--- dev/js/ng-directives.js
+++ dev/js/ng-directives.js
@@ -4,7 +4,7 @@ angular.module('app.directives', [])
 
 	/**
 	 * The Image Uploader directive will allow for a dropzone
-	 * to add the image as Base64 Encoded data URI to the `formData`
+	 * to add the image data URI (Base64 Encoded) to the `formData` scope
 	 * that was passed into it.
 	 *
 	 * Attributes:
@@ -15,7 +15,7 @@ angular.module('app.directives', [])
 	 * Use:
 	 * 	<div
 	 * 		imageUploader
-	 * 		form-data="form"
+	 * 			form-data="form"
 	 * 			[drag-class="dragging-class"]
 	 * 			[image-uploader-image="{{image.url}}"] >
 	 * 	</div>
@@ -38,9 +38,7 @@ angular.module('app.directives', [])
 				var fileInput = jQuery('<input type="file">');
 				var reader = new FileReader();
 
-				/**
-				 * Populate image field if attribute is present
-				 */
+				/** Pre-populate image field if attribute is present **/
 				if(attrs.imageUploaderImage) {
 					scope.$watch('formData', function(newVal) {
 						if(newVal) {
@@ -49,9 +47,6 @@ angular.module('app.directives', [])
 					});
 				}
 
-				/**
-				 * Add file to Form Data via FileReader API
-				 */
 				function addFile(file) {
 					if(file.type.match('image.*')) {
 						reader.onload = function(e) {
@@ -66,26 +61,13 @@ angular.module('app.directives', [])
 						reader.readAsDataURL(file);
 					}
 				}
-
-				/**
-				 * Bring in File Selection Dialog when clicking dropzone
-				 */
 				elem.on('click', function() {
 					fileInput.trigger('click');
 				});
-
-				/**
-				 * Add file to Form Data after selection
-				 */
 				fileInput.on('change', function() {
 					file = fileInput[0].files[0];
 					addFile(file);
 				});
-
-				/**
-				 * Stop browser from opening with file:// protocol
-				 * Add defined dragClass when dragging
-				 */
 				elem.on('dragover', function(event) {
 					event.stopPropagation();
 					event.preventDefault();
@@ -94,10 +76,6 @@ angular.module('app.directives', [])
 						elem.addClass(attrs.dragClass);
 					}
 				});
-
-				/**
-				 * Remove defined dragClass when no longer dragging
-				 */
 				elem.on('dragleave', function(event) {
 					event.stopPropagation();
 					event.preventDefault();
@@ -106,11 +84,6 @@ angular.module('app.directives', [])
 						elem.removeClass(attrs.dragClass);
 					}
 				});
-
-				/**
-				 * Stop browser from opening with file:// protocol
-				 * Add file to Form Data after dropping
-				 */
 				elem.on('drop', function(event) {
 					event.stopPropagation();
 					event.preventDefault();
@@ -165,7 +138,6 @@ angular.module('app.directives', [])
 					$scope.editBarMenu.show = !$scope.editBarMenu.show;
 					$scope.editBarMenu.confirm = false;
 				};
-
 				$scope.deleteGallery = function(gallery, index) {
 					if($scope.editBarMenu.confirm) {
 						gallery.$delete().then(function() {
@@ -212,7 +184,6 @@ angular.module('app.directives', [])
 					$scope.editBarMenu.show = !$scope.editBarMenu.show;
 					$scope.editBarMenu.confirm = false;
 				};
-
 				$scope.deleteEntry = function(entry, index) {
 					if($scope.editBarMenu.confirm) {
 						entry.$delete().then(function() {
@@ -259,7 +230,6 @@ angular.module('app.directives', [])
 					$scope.editBarMenu.show = !$scope.editBarMenu.show;
 					$scope.editBarMenu.confirm = false;
 				};
-
 				$scope.deleteEntry = function(video, index) {
 					if($scope.editBarMenu.confirm) {
 						video.$delete().then(function() {
@@ -298,7 +268,6 @@ angular.module('app.directives', [])
 	})
 
 	.directive( 'videoPlayer', ['$rootScope',	function ($rootScope) {
-
 		return {
 			templateUrl: 'partials/templates/video-player.html',
 			restrict: 'AE',
@@ -311,19 +280,23 @@ angular.module('app.directives', [])
 			},
 			link: function(scope, elem, attrs) {
 
-				/**
-				 * Setup Video
-				 */
+				/** Setup Video **/
 				var video = elem.find('video');
 				var src = 'scope.' + video[0].getAttribute('data-url-model');
 				video[0].src = eval(src);
 				var videoElement = new MediaElement( video[0], {
 					success: function(mediaElement) {
 						mediaElement.addEventListener('ended', function(e) {
-							scope.$apply(function() { scope.video.playing = false; $rootScope.hideMenu = false; });
+							scope.$apply(function() {
+								scope.video.playing = false;
+								$rootScope.hideMenu = false;
+							});
 						}, false);
 						mediaElement.addEventListener('pause', function(e) {
-							scope.$apply(function() { scope.video.playing = false; $rootScope.hideMenu = false; });
+							scope.$apply(function() {
+								scope.video.playing = false;
+								$rootScope.hideMenu = false;
+							});
 						}, false);
 						mediaElement.addEventListener('play', function(e) {
 							$rootScope.$broadcast('PAUSE-OTHER-VIDEOS', {video: scope.video});
@@ -332,9 +305,10 @@ angular.module('app.directives', [])
 								$rootScope.hideMenu = true;
 							});
 
-							// Scroll if on left edge of screen, to account for Menu hiding
+							/** Scroll if on left edge of screen, to account for Menu hiding **/
 							var contentView = angular.element(document.querySelector('.content-full'));
-							var contentViewMargin = parseInt(window.getComputedStyle(contentView[0]).marginLeft);
+							var contentViewMargin =
+										parseInt(window.getComputedStyle(contentView[0]).marginLeft);
 							if(window.scrollX < contentViewMargin-8 ) {
 								setTimeout(function() {
 									window.scrollTo(contentViewMargin-8, 0);
@@ -347,24 +321,19 @@ angular.module('app.directives', [])
 					}
 				});
 
-				/**
-				 * Broadcasted Event Listening: pause video
-				 * if other videos playing
-				 */
+				 /** Broadcasted Event Listening: pause video if other videos playing **/
 				scope.$on('PAUSE-OTHER-VIDEOS', function(event, args) {
 					if( args.video != scope.video ) {
 						videoElement.pause();
 					}
 				});
 
-				/**
-				 * Play/Pause Events
-				 */
+				/** Play/Pause Events **/
 				var playButton = angular.element(elem[0].querySelector('.play-button-js'));
 				var pauseButton = angular.element(elem[0].querySelector('.pause-button-js'));
 				playButton.bind('click', function() {
 					videoElement.play();
-					scope.video.played = true; // signifies first play
+					scope.video.played = true; /** signifies first play **/
 				});
 				pauseButton.bind('click', function() {
 					videoElement.pause();
